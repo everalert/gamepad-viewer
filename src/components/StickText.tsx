9cf816d06ccde2ecc2d9b59db4dd3761f7480711
everalert <everalert@gmail.com>
2023-07-26 00:52:40 +1000

    fix stick text alignment

--- src/components/StickText.tsx
+++ src/components/StickText.tsx
@@ -16,21 +16,19 @@ const StickText = (props: StickTextProps) => {
 	const [params] = useSearchParams();
 	const MODE_COMPACT = params.compact !== undefined || params.lesstext !== undefined
 
-	return <div class='grid grid-cols-2 leading-[1.0125rem]
-		px-3 py-2 pb-1 bg-black/[0.65] rounded-md'>
+	return <div class={`grid ${ MODE_COMPACT ? 'grid-cols-1 w-[6.3rem]' : 'grid-cols-2 w-[10.75rem]' } leading-[1.0125rem] px-3 py-2 pb-1 bg-black/[0.65] rounded-md`}>
 
-		{ props.label || <div
-			class='col-span-2 font-bold text-sm'>
-		{props.label}
-		</div> }
+		{ props.label ? <div class='col-span-full font-bold text-sm'>
+			{props.label}
+		</div> : null }
 
-		<div class={`font-medium ${!MODE_COMPACT || 'col-span-2'}`}>
+		<div class='font-medium'>
 			{numf(props.x,true)}<span class='ml-0.5 text-sm font-normal'>X</span>
 		</div>
 		{ MODE_COMPACT || <div class='text-right text-sm'>
 			{numf(mag(props.y, props.x)>deadzone?ang(-props.y, props.x):0,true,2)}&deg;
 		</div> }
-		<div class={`font-medium ${!MODE_COMPACT || 'col-span-2'}`}>
+		<div class='font-medium'>
 			{numf(props.y,true)}<span class='ml-0.5 text-sm font-normal'>Y</span>
 		</div>
 		{ MODE_COMPACT || <div class='text-right text-sm'>
