26d63eadc8f24341025b5a6d11892fcd7e422732
everalert <everalert@gmail.com>
2023-07-25 14:49:41 +1000

    rework pagelink generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,38 @@
 import type { Component } from 'solid-js';
-import { createSignal } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import { A } from '@solidjs/router';
 
 
-const stupidLinkGen = (uri:string, notext:boolean, noimage:boolean, compact:boolean, lesstext:boolean) => `${uri}?${notext?'notext':''}&${noimage?'noimage':''}&${compact?'compact':''}&${lesstext?'lesstext':''}`
+const PAGES: string[] = ['xbox', 'psx', 'wiiu']
+const PARAMS: string[] = ['notext', 'noimage', 'compact', 'lesstext']
+
+const urigen = (uri:string, params:{[key:string]:boolean}) => {
+	const active: string[] = Object.keys(params).filter(k => params[k] === true)
+	return `${uri}${ active.length > 0 ? `?${active.join('&')}` : '' }`
+}
 
 
 const Index: Component = () => {
-	const [notext, setNotext] = createSignal<boolean>(false)
-	const [noimage, setNoimage] = createSignal<boolean>(false)
-	const [compact, setCompact] = createSignal<boolean>(false)
-	const [lesstext, setLesstext] = createSignal<boolean>(false)
-	return <div class='p-[32px] min-h-screen select-none bg-black'>
-		<div class='mb-4 flex gap-4 justify-center'>
-			<label class={ notext() ? 'text-gray-200' : 'text-gray-400'}>
-				<input type='checkbox' checked={notext()}
-					onclick={()=>{setNotext(!notext())}} /> notext</label>
-			<label class={ noimage() ? 'text-gray-200' : 'text-gray-400'}>
-				<input type='checkbox' checked={noimage()}
-					onclick={()=>{setNoimage(!noimage())}} /> noimage</label>
-			<label class={ compact() ? 'text-gray-200' : 'text-gray-400'}>
-				<input type='checkbox' checked={compact()}
-					onclick={()=>{setCompact(!compact())}} /> compact</label>
-			<label class={ lesstext() ? 'text-gray-200' : 'text-gray-400'}>
-				<input type='checkbox' checked={lesstext()}
-					onclick={()=>{setLesstext(!lesstext())}} /> lesstext</label>
+	const [params, setParams] = createSignal<{[key:string]:boolean}>(
+		PARAMS.reduce((a:any, p:string) => { return {...a, [p]:false}}, {}))
+
+	return <div class='flex gap-8 justify-center p-[32px] min-h-screen select-none bg-black'>
+		<div class='flex flex-col gap-1 justify-start'>
+			<div class='font-semibold'>options</div>
+			<For each={Object.keys(params())}>{p => (
+				<label class={ params()[p] ? 'text-gray-200' : 'text-gray-400'}>
+					<input type='checkbox' checked={params()[p]}
+						onclick={()=>{setParams({...params(),[p]:!params()[p]})}} /> {p}</label>
+			)}</For>
 		</div>
-		<div class='flex gap-4 justify-center font-bold'>
-			<A class='px-2 text-blue-300 bg-blue-950 hover:text-blue-100 hover:bg-blue-900 rounded'
-				href={stupidLinkGen('/xbox',notext(),noimage(),compact(),lesstext())}>XBox</A>
-			<A class='px-2 text-blue-300 bg-blue-950 hover:text-blue-100 hover:bg-blue-900 rounded'
-				href={stupidLinkGen('/psx',notext(),noimage(),compact(),lesstext())}>PSx</A>
-			<A class='px-2 text-blue-300 bg-blue-950 hover:text-blue-100 hover:bg-blue-900 rounded'
-				href={stupidLinkGen('/wiiu',notext(),noimage(),compact(),lesstext())}>WiiU</A>
+		<div class='flex flex-col gap-1 justify-start'>
+			<div class='font-semibold'>layouts</div>
+			<For each={PAGES}>{p => <A
+				class='px-2 font-bold text-blue-300 bg-blue-950 rounded
+				hover:text-blue-100 hover:bg-blue-900'
+				href={urigen(`/${p}`, params())}>
+				{p}	
+			</A>}</For>
 		</div>
 	</div>
 }
