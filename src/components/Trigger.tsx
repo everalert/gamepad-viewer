e4d69b9c922b90feea971f884bae1c3a25f46768
everalert <everalert@gmail.com>
2023-07-28 20:36:04 +1000

    rework trigger, fix trigger alignment

--- src/components/Trigger.tsx
+++ src/components/Trigger.tsx
@@ -14,59 +14,71 @@ interface TriggerProps {
 	class?: string;
 }
 
+const MARK_HSCALE = 2.25
 const MARK_VSCALE = 4/7
+const MARK_RSCALE = 1/3
 
 
-const Trigger = (props: TriggerProps) => <svg
-	version='1.1' xmlns='http://www.w3.org/2000/svg'
-	width={rc2s(props.trigR,props.trigH) + props.markW + props.line*2}
-	height={props.trigH + props.markW + props.line*2}
-	class={`${props.class||''}`}
-	style={`margin-left:-${(props.line*2+props.markW)/2}px;
-		margin-top:-${(props.line*2+props.markW+props.trigH)/2}px;
-		${props.style||''}`}
-	>
-	<path
-		class={`fill-transparent stroke-black/[0.35]`}
-		stroke-width={props.line*3}
-		d={`M ${props.line+rc2s(props.trigR,props.trigH+props.line)+props.markW/2} ${props.markW/2}
-			a ${props.trigR} ${props.trigR} 0  0 0  0 ${props.trigH+props.line*2}`}
-	/>
-	<path
-		class={`fill-transparent ${props.bumper?'stroke-gray-300':'stroke-gray-800'}`}
-		stroke-width={props.line}
-		d={`M ${props.line+rc2s(props.trigR,props.trigH)+props.markW/2} ${props.line+props.markW/2}
-			a ${props.trigR} ${props.trigR} 0  0 0  0 ${props.trigH}`}
-	/>
-	<svg
-		x={props.line/2-props.line*MARK_VSCALE+props.trigR*(1-Math.cos(rc2rad(props.trigR,props.trigH)*(props.trigger-0.5)))}
-		y={props.line/2-props.line*MARK_VSCALE+props.trigH/2-
-			props.trigR*(Math.sin(rc2rad(props.trigR,props.trigH)*(props.trigger-0.5)))}
-		width={props.markW+(props.line+props.line*MARK_VSCALE)*2}
-		height={props.markW+(props.line+props.line*MARK_VSCALE)*2}
+const Trigger = (props: TriggerProps) => {
+	const m = () => props.line*MARK_HSCALE
+	const rad = () => rc2rad(props.trigR,props.trigH)
+	const deg = () => rc2deg(props.trigR,props.trigH)
+	const markL = () => props.line*MARK_VSCALE
+	const markW = () => markL()+m()
+	const markH = () => markL()+m()*MARK_VSCALE
+	const markR = () => markW()*MARK_RSCALE
+	const markContW = () => Math.sqrt(markW()**2+markW()**2)
+	const trig = () => props.trigger-0.5
+	const linerad = () => rad()+(props.line/4)/(2*props.trigR*Math.PI)*(180/Math.PI)
+
+	return <svg
+		version='1.1' xmlns='http://www.w3.org/2000/svg'
+		width={rc2s(props.trigR,props.trigH) + m()*2}
+		height={props.trigH + m()*2}
+		class={`${props.class||''}`}
+		style={`margin-left:-${m()}px;
+margin-top:-${m()+props.trigH/2}px;
+${props.style||''}`}
 		>
-		<rect
-			class='fill-black'
-			transform={`rotate(${rc2deg(props.trigR,props.trigH)*(props.trigger-0.5)} ${props.markW/2+(props.line*MARK_VSCALE)} ${props.markW/2+(props.line*MARK_VSCALE)})`}
-			x={props.line-props.line*MARK_VSCALE}
-			y={props.line-props.line*MARK_VSCALE+(props.markW-props.markW*MARK_VSCALE)/2}
-			rx={props.markW*MARK_VSCALE**2+props.line*MARK_VSCALE}
-			ry={props.markW*MARK_VSCALE**2+props.line*MARK_VSCALE}
-			width={props.line*MARK_VSCALE*2+props.markW}
-			height={props.line*MARK_VSCALE*2+props.markW*MARK_VSCALE}
+		<path
+			// outline
+			class={`fill-transparent stroke-black/[0.35]`}
+			stroke-width={props.line*3}
+			d={`M
+				${m()+props.trigR*(1-Math.cos(linerad()/2))}
+				${m()+props.trigH/2-props.trigR*Math.sin(linerad()/2)}
+				a ${props.trigR} ${props.trigR} 0  0 0  0 ${props.trigR*Math.sin(linerad()/2)*2}`}
 		/>
-		<rect
-			class='fill-white'
-			transform={`rotate(${rc2deg(props.trigR,props.trigH)*(props.trigger-0.5)} ${props.markW/2+(props.line*MARK_VSCALE)} ${props.markW/2+(props.line*MARK_VSCALE)})`}
-			x={props.line}
-			y={props.line+(props.markW-props.markW*MARK_VSCALE)/2}
-			rx={props.markW*MARK_VSCALE**2}
-			ry={props.markW*MARK_VSCALE**2}
-			width={props.markW}
-			height={props.markW*MARK_VSCALE}
+		<path
+			// main line
+			class={`fill-transparent ${props.bumper?'stroke-gray-300':'stroke-gray-800'}`}
+			stroke-width={props.line}
+			d={`M
+				${m()+props.trigR*(1-Math.cos(rad()/2))}
+				${m()+props.trigH/2-props.trigR*Math.sin(rad()/2)}
+				a ${props.trigR} ${props.trigR} 0  0 0  0 ${props.trigH}`}
 		/>
+		<svg
+			// marker 
+			x={m()-markContW()/2+props.trigR*(1-Math.cos(rad()*trig()))}
+			y={m()-markContW()/2+props.trigH/2-props.trigR*(Math.sin(rad()*trig()))}
+			width={markContW()}
+			height={markContW()}
+			>
+			<rect
+				class='fill-white stroke-black'
+				stroke-width={markL()}
+				transform={`rotate(${deg()*trig()} ${markContW()/2} ${markContW()/2})`}
+				x={(markContW()-markW())/2}
+				y={(markContW()-markH())/2}
+				rx={markR()}
+				ry={markR()}
+				width={markW()}
+				height={markH()}
+			/>
+		</svg>
 	</svg>
-</svg>
+}
 
 export const WTrigger = (props: WidgetProps): JSXElement => <Widget
 	widget={props.def} container={props.container}>
