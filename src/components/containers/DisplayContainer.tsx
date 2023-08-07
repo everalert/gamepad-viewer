46a7ae2e37e63caf1392facecc3aa6d078c2c01f
everalert <everalert@gmail.com>
2023-08-07 21:38:34 +1000

    allow text to expand layout

--- src/components/containers/DisplayContainer.tsx
+++ src/components/containers/DisplayContainer.tsx
@@ -25,17 +25,15 @@ export const DisplayContainer = (props: DisplayContainerProps): JSXElement => {
 	const MODE_EDIT	= location.pathname === '/custom/edit'
 
 	return <div
-			class={`flex flex-col items-center
+			class={`inline-flex flex-col items-center
 				${MODE_EDIT ? 'bg-black min-w-full min-h-screen' :
 				(NOGUIDE ? '' : 'outline outline-[4px] outline-red-500')}`}
 			style={`
-				width:${props.container.w+props.container.m*2}px;
 				padding:${props.container.m}px;
 				gap:${props.container.m/2}px;
 				`}>
 		{ NOTEXT || <TextContainer
 			widgets={props.widgets} pad={props.pad()}
-			style={`width:${props.container.w}px;`}
 			class={`${!MODE_EDIT||'outline outline-[4px] outline-gray-800'}`}/>
 		}
 		{ NOIMAGE || <WidgetContainer
