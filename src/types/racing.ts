7fb2f41db457d7f658db446fa517d7b882ba1dc3
everalert <everalert@gmail.com>
2023-08-07 18:09:14 +1000

    tweak racing design

--- src/types/racing.ts
+++ src/types/racing.ts
@@ -5,16 +5,17 @@ import { TriggerSimpleMode } from '../components/Trigger'
 import { XBoxButton as XBB } from './xbox'
 
 
-const INNER_X	= 156
-const OUTER_X	= 176
-const STICK_Y	= -12
-const BUTTON_Y	= 8
+const INNER_X	= 208
+const OUTER_X	= 248
+const STICK_Y	= -22
+const BUTTON_Y	= 14
+
 
 export const RACING_DFLT_CONTAINER: WidgetContainerDef = {
 	w: 512,
 	h: 64,
 	m: 32,
-	line: 3
+	line: 6
 }
 
 export const RACING_DFLT_WIDGETS: WidgetDef[] = [
@@ -49,7 +50,7 @@ export const RACING_DFLT_WIDGETS: WidgetDef[] = [
 			inputDef(GIT.Button,XBB.LT),
 			inputDef(GIT.Button,XBB.LB),
 		],
-		val: [56,128,0]
+		val: [76,128,TriggerSimpleMode.FullThick]
 	},
 	{
 		type: WidgetType.Trigger,
@@ -59,53 +60,53 @@ export const RACING_DFLT_WIDGETS: WidgetDef[] = [
 			inputDef(GIT.Button,XBB.RT),
 			inputDef(GIT.Button,XBB.RB),
 		],
-		val: [56,128,0],
+		val: [76,128,TriggerSimpleMode.FullThick],
 		fx: true
 	},
 	
 	{
 		type: WidgetType.Trigger,
-		x: -INNER_X-RACING_DFLT_CONTAINER.line*1.175,
+		x: -INNER_X-RACING_DFLT_CONTAINER.line-2,
 		y: 0,
 		inputs: [
 			inputDef(GIT.Axis,0),
 			inputDef(GIT.Button,XBB.LSB)
 		],
-		val: [52,128+RACING_DFLT_CONTAINER.line*2.35,TriggerSimpleMode.Split],
+		val: [68,0,TriggerSimpleMode.Split],
 	},
 	{
 		type: WidgetType.Trigger,
-		x: -INNER_X+RACING_DFLT_CONTAINER.line*1.175,
+		x: -INNER_X+RACING_DFLT_CONTAINER.line+2,
 		y: 0,
 		inputs: [
 			inputDef(GIT.Axis,1),
 			inputDef(GIT.Button,XBB.LSB)
 		],
-		val: [48,128,TriggerSimpleMode.Split],
+		val: [68,0,TriggerSimpleMode.Split],
 		fy: true
 	},
 	
 	{
 		type: WidgetType.Trigger,
-		x: INNER_X+RACING_DFLT_CONTAINER.line*1.175,
+		x: INNER_X+RACING_DFLT_CONTAINER.line+2,
 		y: 0,
 		inputs: [
 			inputDef(GIT.Axis,2),
-			inputDef(GIT.Button,XBB.LSB)
+			inputDef(GIT.Button,XBB.RSB)
 		],
-		val: [52,128+RACING_DFLT_CONTAINER.line*2.35,TriggerSimpleMode.Split],
+		val: [68,0,TriggerSimpleMode.Split],
 		fx: true,
 		fy: true
 	},
 	{
 		type: WidgetType.Trigger,
-		x: INNER_X-RACING_DFLT_CONTAINER.line*1.175,
+		x: INNER_X-RACING_DFLT_CONTAINER.line-2,
 		y: 0,
 		inputs: [
 			inputDef(GIT.Axis,3),
-			inputDef(GIT.Button,XBB.LSB)
+			inputDef(GIT.Button,XBB.RSB)
 		],
-		val: [48,128,TriggerSimpleMode.Split],
+		val: [68,0,TriggerSimpleMode.Split],
 		fx: true,
 		fy: true
 	},
@@ -116,7 +117,7 @@ export const RACING_DFLT_WIDGETS: WidgetDef[] = [
 		y: STICK_Y,
 		rot: 90,
 		inputs: [inputDef(GIT.Axis,0)],
-		val: [276,0,TriggerSimpleMode.SplitThick]
+		val: [374,0,TriggerSimpleMode.SplitThick]
 	},
 	{
 		type: WidgetType.ButtonGridRect,
@@ -134,7 +135,7 @@ export const RACING_DFLT_WIDGETS: WidgetDef[] = [
 			inputDef(GIT.Button,14),
 			inputDef(GIT.Button,15),
 		],
-		val: [10,28,0,12,10,0,1]
+		val: [10,38,0,16,20,0,1],
 	},
 ]
 
