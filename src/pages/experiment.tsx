821a36cad8fe6308751cfde19ce440e1c3b9adcd
everalert <everalert@gmail.com>
2023-11-30 12:58:40 +1100

    steering page layout adjustment

--- src/pages/experiment.tsx
+++ src/pages/experiment.tsx
@@ -9,9 +9,12 @@ import { ang, mag, clamp } from '../helpers/math'
 import { numf } from '../helpers/formatting'
 
 
+const WIDGET_COLOR = Color.Blue
+
+
 export const ExperimentPage: Component = () => {
-	const [angleMax, setAngleMax] = createSignal<number>(90)
-	const [angleMir, setAngleMir] = createSignal<boolean>(false)
+	const [angleMax, setAngleMax] = createSignal<number>(135)
+	const [angleOver, setAngleOver] = createSignal<boolean>(true)
 	const [dzSt, setDzSt] = createSignal<number>(4)
 	const [dzMid, setDzMid] = createSignal<number>(0.05)
 	const [dzOut, setDzOut] = createSignal<number>(0.03)
@@ -31,61 +34,72 @@ export const ExperimentPage: Component = () => {
 		((clamp(a()*as()-angleMax(),dzSt(),angleMax()-dzSt())-dzSt()) /
 			(angleMax()-dzSt()*2))
 	const steer = () => {
-		const angle = angleMir() ? angle_core()-angle_over() : angle_core()
+		const angle = angleOver() ? angle_core() : angle_core()-angle_over()
 		const amount = mdz()
 		return angle * amount
 	}
 
 	return <div
-		class='w-screen h-screen flex gap-4 items-center place-content-center'
+		class='w-screen h-screen grid grid-auto-cols auto-col-min gap-16 items-center place-content-center'
 		>
-		<div class='grid grid-cols-2 auto-cols-min'>
-			<span>{numf(a(),true,3)}&deg;</span><span>ang in</span>
-			<span>{numf(angle_core())}</span><span>ang out core</span>
-			<span>{numf(angle_over())}</span><span>ang out over</span>
-			<span>{numf(m())}</span><span>mag in</span>
-			<span>{numf(mdz())}</span><span>mag out</span>
-			<span>{numf(steer(),true)}</span><span>output</span>
+		<div
+			class='relative col-span-2 grid grid-cols-2'
+			>
+			<span class='col-span-2 text-center font-bold'>output</span>
+			<div class='col-span-2 relative left-[50%] top-[24px] h-[48px]'>
+				<Trigger
+					trigger	= { steer() / 2 + 0.5 }
+					bumper	= { false }
+					trigH	= { 96*4 }
+					trigR	= { 0 }
+					simple	= { TriggerSimpleMode.Split }
+					line	= { 12 }
+					color	= { WIDGET_COLOR }
+					style	= 'transform: rotate(90deg);'
+				/>
+			</div>
+			<span class={`${steer()<0?'':'opacity-10'} px-2 text-right`}>
+				{steer()<0 ? numf(steer()) : '0.0000'}
+			</span>
+			<span class={`${steer()>0?'':'opacity-10'} px-2`}>
+				{steer()>0 ? numf(steer()) : '0.0000'}
+			</span>
 		</div>
 
-		<div
-			class='relative'
-			style='width:256px;height:224px;'
-			>
-			<Stick
-				x		= { x() }
-				y		= { y() }
-				button	= { false }
-				r		= { 64 }
-				a		= { 67.5 }
-				ar		= { 64 }
-				simple	= { false }
-				line	= { 3 }
-				color	= { Color.Default }
-				class	= 'absolute'
-				style	= 'left:128; top:96px;'
-			/>	
-			<Trigger
-				trigger	= { steer() / 2 + 0.5 }
-				bumper	= { false }
-				trigH	= { 96*2 }
-				trigR	= { 0 }
-				simple	= { TriggerSimpleMode.Split }
-				line	= { 6 }
-				color	= { Color.Default }
-				class	= 'absolute'
-				style	= 'left:128px; top:192px; transform: rotate(90deg);'
-			/>
+		<div class='flex flex-col gap-8 items-center'>
+			<div
+				class='relative w-[128px] h-[128px]'
+				>
+				<Stick
+					x		= { x() }
+					y		= { y() }
+					button	= { false }
+					r		= { 64 }
+					a		= { 67.5 }
+					ar		= { 64 }
+					simple	= { false }
+					line	= { 3 }
+					color	= { WIDGET_COLOR }
+					class	= 'absolute left-[64px] top-[64px]'
+				/>	
+			</div>
+			<div class='grid grid-cols-[4.75rem_max-content] gap-x-4'>
+				<span class='text-right'>{numf(a(),true,3)}&deg;</span><span>ang in</span>
+				<span class='text-right'>{numf(angle_core())}</span><span>ang out core</span>
+				<span class='text-right'>{numf(angle_over())}</span><span>ang out over</span>
+				<span class='text-right'>{numf(m())}</span><span>mag in</span>
+				<span class='text-right'>{numf(mdz())}</span><span>mag out</span>
+			</div>
 		</div>
 
 		<div
 			class='flex flex-col gap-2'
 			>
 			<Checkbox
-				label='oversteer mirror'
-				tooltip='oversteering brings output back toward neutral'
-				value={angleMir()}
-				setValFn={setAngleMir}
+				label='oversteer'
+				tooltip='steering beyond full-steer angle maintains max steering'
+				value={angleOver()}
+				setValFn={setAngleOver}
 			/>
 			<Slider
 				label='full-steer angle'
